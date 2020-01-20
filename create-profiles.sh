560b25419c472beace0145d420827b1030751a9b
HiFiBerry <info@hifiberry.com>
2020-01-20 16:54:41 +0100

    Create 96k profiles

--- create-profiles.sh
+++ create-profiles.sh
@@ -1,6 +1,12 @@
 cp hifiberry-default.xml hifiberry-default-4ca.xml
 cp hifiberry-default.xml hifiberry-default-dac.xml
+cp hifiberry-96k.xml hifiberry-96k-dac.xml
+
 dsptoolkit store-settings params-4ca.txt hifiberry-default-4ca.xml
 dsptoolkit store-settings params-dac.txt hifiberry-default-dac.xml
+dsptoolkit store-settings params-dac.txt hifiberry-96k-dac.xml
+
 sed -i 's/Universal/Universal Beocreate/' hifiberry-default-4ca.xml
 sed -i 's/Universal/Universal DSP/' hifiberry-default-dac.xml
+sed -i 's/96k/DAC+ DSP 96k/' hifiberry-96k-dac.xml
+
